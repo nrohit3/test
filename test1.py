b56f55d3e439732f935acb6afb7adaafad2d91c7
unknown <nrohit3@gmail.com>
2022-07-17 07:26:38 +0530

    Added Files to GIT

---
+++ test1.py
@@ -0,0 +1,8 @@
+#read data from table
+import mysql.connector as con
+
+connect=con.connect(host='localhost',port=3306,user='root',passwd='Feb2018a')
+cur=connect.cursor(buffered=True)
+q1='select * from r1pred.good_raw_data'
+cur.execute(q1)
+print(cur.fetchall())
